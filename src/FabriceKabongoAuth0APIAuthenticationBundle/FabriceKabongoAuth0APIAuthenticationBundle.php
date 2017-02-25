df8a0da7a1a9a09ab94081cf447761bdb2926408
Fabrice Kabongo <fabrice.k.kabongo@gmail.com>
2017-02-25 03:55:20 +0200

    manually load the extension

--- src/FabriceKabongoAuth0APIAuthenticationBundle/FabriceKabongoAuth0APIAuthenticationBundle.php
+++ src/FabriceKabongoAuth0APIAuthenticationBundle/FabriceKabongoAuth0APIAuthenticationBundle.php
@@ -9,9 +9,18 @@
 namespace FabriceKabongo\Auth0\APIAuthenticationBundle;
 
 
+use FabriceKabongo\Auth0\APIAuthenticationBundle\DependencyInjection\FabriceKabongoAuth0APIAuthenticationBundleExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class FabriceKabongoAuth0APIAuthenticationBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        if (is_null($this->extension)) {
+            $this->extension = new FabriceKabongoAuth0APIAuthenticationBundleExtension();
+        }
+
+        return $this->extension;
+    }
 
 }
